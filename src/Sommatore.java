7aa7dd799eda6296e812ccded0f997ac02278466
matteo <mat.mensi@gmail.com>
2018-02-19 09:00:12 +0100

    aggiunto somma 10

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -1,8 +1,28 @@
 
-public class Sommatore {
+public class Sommatore 
+{
 
-	public static void main(String[] args) {
+	public static int somma4(int x)
+	{
+		for (int i = 0; i < 4; i++) 
+			x++;
+		return x;
+	}
+	
+	public static int somma10(int x)
+	{
+		for (int i = 0; i < 10; i++) 
+			x++;
+		return x;
+	}
+	
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		
+		int x=1;
+		System.out.println(somma4(x));
+		System.out.println(somma10(x));
 
 	}
 
